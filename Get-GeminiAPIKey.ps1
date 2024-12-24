4ff4d60634e545c835a2d50333893195f40794b2
TeamDman <TeamDman9201@gmail.com>
2024-12-23 23:30:53 -0500

    feat: add ollama_chat model settings and update environment checks

--- Get-GeminiAPIKey.ps1
+++ Get-GeminiAPIKey.ps1
@@ -1,4 +1,4 @@
-if (-not (Test-Path -Path Env:\DATABASE_URL)) {
+if (-not (Test-Path -Path Env:\GEMINI_API_KEY)) {
     Write-Host "[Get-GeminiAPIKey] Make sure to dot-source this file!"
     $api_key = op item get "Nanuak Gemini Aider API key" --vault "Private" --field credential
     $env:GEMINI_API_KEY=$api_key
