551b84fcc908ab66e1b0a862467f77f22ce7362e
TeamDman <TeamDman9201@gmail.com>
2024-12-23 19:18:16 -0500

    Fix new-migration script to open the sql files automatically

--- nanuak-schema/New-Migration.ps1
+++ nanuak-schema/New-Migration.ps1
@@ -1,2 +1,7 @@
 $name = Read-Host "Enter the name of the migration"
 diesel migration generate $name
+$migration = Get-ChildItem .\migrations `
+| Where-Object { $_.Name -like "*$name*"} `
+| Select-Object -First 1 -ExpandProperty Name
+code ".\migrations\$migration\up.sql"
+code ".\migrations\$migration\down.sql"
