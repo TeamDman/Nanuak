4bcc5659e3d09f0eed377816de3fcd5e06752336
TeamDman <TeamDman9201@gmail.com>
2025-02-01 15:44:19 -0500

    add guard to redo-migrations

--- nanuak-schema/Redo-Migration.ps1
+++ nanuak-schema/Redo-Migration.ps1
@@ -1,2 +1,7 @@
 . .\Get-DatabaseUrl.ps1
-diesel migration redo
+diesel migration list
+# ask are you sure
+$resp = Read-Host "Are you sure you want to undo the last migration? (y/n)"
+if ($resp -eq "y") {
+    diesel migration redo
+}
