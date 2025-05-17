f5177caf098c191cfe2d3e89cc14197e32ea94d0
TeamDman <TeamDman9201@gmail.com>
2025-05-16 21:51:38 -0400

    avoid rebuilding if already have binary

--- nanuak-youtube-embeddings/Invoke-SearchVideoEmbeddings.ps1
+++ nanuak-youtube-embeddings/Invoke-SearchVideoEmbeddings.ps1
@@ -1,1 +1,12 @@
-cargo run --bin search_video_embeddings
+$releaseExe = "target/release/search_video_embeddings.exe"
+$debugExe = "target/debug/search_video_embeddings.exe"
+
+if (Test-Path $releaseExe) {
+    & $releaseExe
+}
+elseif (Test-Path $debugExe) {
+    & $debugExe
+}
+else {
+    cargo run --bin search_video_embeddings
+}
